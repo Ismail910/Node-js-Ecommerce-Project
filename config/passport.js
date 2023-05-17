8e57e5ca05693eb91797efb6e5e6510322ba13c8
Ismail910 <ismailmagdy566@gmail.com>
2023-05-17 03:08:38 +0300

    move to new repo

---
+++ config/passport.js
@@ -0,0 +1,186 @@
+// config/passport.js
+
+// load all the things we need
+var LocalStrategy = require("passport-local").Strategy;
+
+// Generate Hash
+var bcrypt = require("bcrypt-nodejs");
+
+// database module
+var database = require("../config/database");
+var RunQuery = database.RunQuery;
+
+module.exports = function (passport) {
+  passport.serializeUser(function (user, done) {
+    done(null, user.UserID);
+  });
+
+  passport.deserializeUser(function (userId, done) {
+    var sqlStr =
+      "\
+            SELECT *\
+            FROM users\
+            where UserID = '" +
+      userId +
+      "'";
+    RunQuery(sqlStr, function (rows) {
+      done(null, rows[0]);
+    });
+  });
+
+  passport.use(
+    "sign-in",
+    new LocalStrategy(
+      {
+        usernameField: "username",
+        passwordField: "password",
+        passReqToCallback: true,
+      },
+      function (req, username, password, done) {
+        var sqlStr = "SELECT * FROM users WHERE Username = '" + username + "'";
+        RunQuery(sqlStr, function (rows) {
+          if (rows.length < 1)
+            return done(
+              null,
+              false,
+              req.flash("signInError", "No user found.")
+            ); // req.flash is the way to set flashdata using connect-flash
+
+          if (!bcrypt.compareSync(password, rows[0].Password))
+            return done(
+              null,
+              false,
+              req.flash("signInError", "Oops! Wrong password.")
+            ); // create the loginMessage and save it to session as flashdata
+
+          return done(null, rows[0]);
+        });
+      }
+    )
+  );
+
+  passport.use(
+    "sign-up",
+    new LocalStrategy(
+      {
+        // by default, local strategy uses username and password
+        usernameField: "username",
+        passwordField: "password",
+        passReqToCallback: true, // allows us to pass back the entire request to the callback
+      },
+      function (req, username, password, done) {
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        var email = req.body.email;
+
+        if (password != req.body.rePassword) {
+          return done(
+            null,
+            false,
+            req.flash("signUpError", "Passwords do not match.")
+          );
+        } else {
+          var selectQuery =
+            "SELECT *\
+                    FROM users\
+                    WHERE email = '" +
+            email +
+            "'";
+          RunQuery(selectQuery, function (emailRows) {
+            if (emailRows.length > 0) {
+              return done(
+                null,
+                false,
+                req.flash("signUpError", "Email address has already been used.")
+              );
+            } else {
+              selectQuery =
+                "\
+                        SELECT *\
+                        FROM users\
+                        WHERE username = '" +
+                username +
+                "'";
+              RunQuery(selectQuery, function (usernameRows) {
+                if (usernameRows.length > 0) {
+                  return done(
+                    null,
+                    false,
+                    req.flash("signUpError", "Username has already been used.")
+                  );
+                } else {
+                  // if there is no user with that user and email
+                  var fullName = req.body.fullName;
+                  var phone = req.body.phone;
+                  var address = req.body.streetAddress;
+                  var postcode = req.body.postcode;
+                  var city = req.body.city;
+                  var country = req.body.country;
+                  var passwordHash = bcrypt.hashSync(password, null, null);
+
+                  var insertQuery =
+                    "INSERT INTO Users\
+                                    VALUES(null,\
+                                    '" +
+                    fullName +
+                    "', \
+                                    '" +
+                    address +
+                    "', \
+                                    '" +
+                    postcode +
+                    "', \
+                                    '" +
+                    city +
+                    "', \
+                                    '" +
+                    country +
+                    "', \
+                                    '" +
+                    phone +
+                    "', \
+                                    '" +
+                    email +
+                    "', \
+                                    '" +
+                    username +
+                    "', \
+                                    '" +
+                    passwordHash +
+                    "', 0)";
+
+                  RunQuery(insertQuery, function (insertResult) {
+                    //user
+                    var user = {
+                      UserID: insertResult.insertId,
+                    };
+                    insertQuery =
+                      "INSERT INTO Addresses\
+                                    VALUES(null, " +
+                      insertResult.insertId +
+                      ", '" +
+                      fullName +
+                      "', '" +
+                      address +
+                      "', '" +
+                      postcode +
+                      "', '" +
+                      city +
+                      "', '" +
+                      country +
+                      "', '" +
+                      phone +
+                      "')";
+                    RunQuery(insertQuery, function (addRow) {
+                      return done(null, user);
+                    });
+                  });
+                }
+              });
+            }
+          });
+        }
+      }
+    )
+  );
+};
